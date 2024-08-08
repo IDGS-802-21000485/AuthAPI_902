cc9dd8b605b8c76a6b3b751e2a999a80ed88acf9
IDGS-802-21000485 <21000485@alumnos.utleon.edu.mx>
2024-08-07 18:00:52 -0600

    Agregar archivos de proyecto.

---
+++ AuthAPI_902/Dtos/CreateRoleDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI_902.Dtos
+{
+    public class CreateRoleDto
+    {
+        [Required(ErrorMessage ="Role Name is required.")]
+        public string RoleName { get; set; } = null!;
+    }
+}
---
+++ AuthAPI_902/Dtos/ForgotPasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI_902.Dtos
+{
+	public class ForgotPasswordDto
+	{
+       [Required]
+       [EmailAddress]
+       public string Email { get; set; } = string.Empty;
+
+    }
+}
+
